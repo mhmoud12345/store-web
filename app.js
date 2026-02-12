16b80123f1d185dac40927000f38707ad6739404
mhmoud12345 <mhmoudabdalrhmn1234@gmail.com>
2026-02-12 05:38:19 +0200

    00

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const http= require('http')
 require("dotenv").config();
-
+const helmet = require('helmet');
 const  express=require('express')
 const  mongoose=require('mongoose')
-
+const compression = require('compression');
 
 const app=express();
 const path = require('path');
@@ -14,7 +14,8 @@ app.set('view engine','ejs')
 app.set('views','views')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(helmet());
+app.use(compression());
 // mongoose.connect('mongodb://localhost/cooking-web').then(()=>{console.log('db connected')}).catch((err)=>{console.log(err)})  
 mongoose.connect(process.env.MONGO_URI).then(() => {console.log('db connected')}).catch((err) => {console.log(err)})
 app.use(express.static(path.join(__dirname,'public')))
@@ -22,12 +23,12 @@ app.use(express.static(path.join(__dirname,'public')))
 
 app.use(mainrouts)
 app.use(cartrouts)
-app.listen(3000,()=>{ console.log('port contected')})
 
+const PORT = process.env.PORT || 3000;
 
-
-
-
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 
 
 
